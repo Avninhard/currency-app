200d5b66741b066d47b78ea24cba4c1507d9ae6e
Avninhard <149249566+Avninhard@users.noreply.github.com>
2026-02-22 10:50:09 +0100

    init: incjalizacja prjektu z .gitignore i podstawowymi plikami generowanymi przez angular

---
+++ frontend/src/app/components/currency-filter/currency-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-filter.component.html',
+  styleUrl: './currency-filter.component.css'
+})
+export class CurrencyFilterComponent {
+
+}
---
+++ frontend/src/app/components/currency-table/currency-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-table.component.html',
+  styleUrl: './currency-table.component.css'
+})
+export class CurrencyTableComponent {
+
+}
